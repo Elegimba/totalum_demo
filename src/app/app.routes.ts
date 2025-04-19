f5808d74146e31d83fdbdfb642b8332f995ae4d4
YagoGV <yagogv@gmail.com>
2025-04-19 16:53:31 +0200

    plantilla de tabla products

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
+import { ProductsComponent } from './pages/products/products.component';
 
 export const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: '/main' },
     { path: 'main', component: MainComponent },
+    { path: 'products', component: ProductsComponent },
     { path: '**', redirectTo: '/main' }
 ];
--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faBox, faCartShopping, faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-main',
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, RouterLink],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
